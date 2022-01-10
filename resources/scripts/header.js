b0bdc59adb0c8f934886376a08fd75e44962bf11
AlikaJam <71496319+AlikaJam@users.noreply.github.com>
2022-01-11 08:25:28 +1000

    homework

---
+++ resources/scripts/header.js
@@ -0,0 +1,8 @@
+const headerMenu = document.querySelector('.menu');
+const headerMenuToggle = document.querySelector('.menu-toggle');
+if (headerMenuToggle) {
+  headerMenuToggle.addEventListener('click', () => {
+    headerMenu.classList.toggle('active');
+    headerMenuToggle.classList.toggle('active');
+  })
+}
